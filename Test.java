417c2351ff7d1d0a0f44ccf83a1b3c24deeb99ee
fcrathod2801 <111195051+fcrathod2801@users.noreply.github.com>
2023-01-16 17:05:49 +0530

    Constructor Changes
    Changes

--- Test.java
+++ Test.java
@@ -1,6 +1,13 @@
 class Test{
   
 private int Id;
+  private String name;
+  
+  public Test(int id,String name){
+    super();
+    this.id=id;
+    this.name=name;
+  }
   public String disData(){
     return "Display Title on MS";
   }
